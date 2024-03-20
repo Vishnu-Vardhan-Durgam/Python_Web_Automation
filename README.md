bb502048463eebf52371241d935bbb07802138da
vishnu vardhan <vardhan.vishnu557@gmail.com>
2024-03-20 19:52:37 +0530

    Web Automation

---
+++ README.md
@@ -0,0 +1,13 @@
+# Web automation repo
+
+## Install the 
+- selenium ( > 4 )
+- allure pytest
+- Pytest html
+- xdist - to run parallel execution
+- logging
+- openpyxl - CSV,Excel
+- faker lib(fake data generate)
+
+'''pip install selenium pytest pytest-html allure-pytest
+'''
---
+++ code/01Basics/test_lab01.py
@@ -0,0 +1,20 @@
+# selenium
+
+# selenium --3
+# 1) you need to set up the browser drivers to machin PC/Mac
+# 2) Problems with browers
+#
+# # selenium --4
+# 1) w3c protocol, selenium manager - which will automatically download the driver browser for you
+
+from selenium import webdriver
+
+def test_open_login():
+    driver = webdriver.Chrome()
+    driver.get("https://www.google.com/")
+    driver.maximize_window()
+    print(driver.title)
+
+
+
+
